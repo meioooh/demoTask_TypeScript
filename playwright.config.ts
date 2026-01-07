7b8f97eb5ceb60556a1de3bb999783928f7e0ae2
meioooh <meixxxh@gmail.com>
2026-01-07 21:41:52 +0300

    added videos and screens on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,12 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-  baseURL: 'https://the-internet.herokuapp.com',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: 'https://the-internet.herokuapp.com',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
