5b6a7d6522620935f7c6beca2825262cf0eeb76f
meioooh <meixxxh@gmail.com>
2026-01-07 22:43:52 +0300

    finished first test "Page title contains"

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('contains title', async ({ page }) => {
+test('Page title contains text', async ({ page }) => {
+    // переход на страницу по указанному URL
     await page.goto('https://the-internet.herokuapp.com/');
+
+    // ожидаем, что в заголовке страницы есть текст 'the-internet'
+    const h1Locator = page.locator('h1.heading');
+    await expect(h1Locator).toContainText('the-internet');
 });
