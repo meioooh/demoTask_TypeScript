998beaff44c5486d0857c86d5cb1a83747232169
meioooh <meixxxh@gmail.com>
2026-01-08 17:46:17 +0300

    finished "Login" test and made some adjustments

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,21 +1,42 @@
 import { test, expect } from '@playwright/test';
 
+//  выполняется перед каждым отдельным тестом
+test.beforeEach(async ({ page }) => {
+  console.log(`Running ${test.info().title}`);
+}); 
+
 // группирование тестов по общему признаку
 test.describe('Simple UI tests', () => {
 
 test('Page title', async ({ page }) => {
-    // переход на страницу по base URL
     await page.goto('/');
-
-    const h1Locator = page.locator('h1.heading');
-    await expect(h1Locator).toContainText('the-internet'); // ожидаем, что в заголовке страницы есть текст 'the-internet'
+    await expect(page.locator('h1.heading')).toContainText('the-internet'); // ожидаем, что в заголовке страницы есть текст 'the-internet'
 });
 
 test('Navigation', async ({ page }) => {
     await page.goto('/');
+
     await page.getByRole('link', { name: 'Form Authentication'}).click(); // переходим по ссылке form authentication
-    await page.waitForURL('**\/login'); // дождались полной загрузки страницы '/login'
+    await page.waitForURL('/login'); // дождались полной загрузки страницы '/login'
+    await expect(page).toHaveURL('/login')
     await expect(page.locator('h2')).toContainText('Login Page'); // страница содержит текст 'Login Page'
+});
+
+test('Login', async ({ page }) => {
+    await page.goto('/login');
+
+    // вводим логин и пароль
+    await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith'); 
+    await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+    
+    // жмем логин и дожидаемся загрузки следующей страницы
+    await page.locator('button:has-text("Login")').click();
+    await page.waitForURL('/secure');
+
+    await expect(page.locator('div#flash.flash.success')).toBeVisible(); // видно сообщение об успехе
+    await expect(page.locator('div#flash.flash.success')).toContainText('You logged into a secure area!'); // ожидаемый текст внутри сообщения
+
+
 });
 
 });
